52b12e3302dc198dc26a9ad5b8e38232567e4630
Art33mis <artyom12149@gmail.com>
2021-03-04 14:40:57 +0300

    v0.1

--- viterbi_dec.py
+++ viterbi_dec.py
@@ -1,14 +1,14 @@
 import math
 
 
-def min_num(a, b):
+def get_min_num(a, b):
     if a >= b:
         return b
     if a < b:
         return a
 
 
-def min_index(cell):
+def get_min_index(cell):
     min_num = math.inf
     min_ind = 0
     for i in range(len(cell)):
@@ -29,29 +29,41 @@ def hamming_distance(str1, str2):
     return distance
 
 
-def decoder(input_str):
-    input_list = input_str.split(" ")
-    trellis = [["00", "11"], ["11", "00"], ["01", "10"], ["10", "01"]]
+def get_cell(input_list, trellis):
     cell = [[math.inf] * 4 for i in range(len(input_list) + 1)]
     cell[0][0] = 0
-    way = [0 for i in range(len(input_list) + 1)]
     for i in range(len(cell)):
         for j in range(4):
             if j == 0 and i != 0:
-                cell[i][j] = min_num(hamming_distance(input_list[i-1], trellis[0][0]) + cell[i - 1][0],
-                                     hamming_distance(input_list[i-1], trellis[0][1]) + cell[i - 1][2])
+                cell[i][j] = get_min_num(hamming_distance(input_list[i - 1], trellis[0][0]) + cell[i - 1][0],
+                                         hamming_distance(input_list[i - 1], trellis[0][1]) + cell[i - 1][2])
             elif j == 1 and i != 0:
-                cell[i][j] = min_num(hamming_distance(input_list[i-1], trellis[1][0]) + cell[i - 1][0],
-                                     hamming_distance(input_list[i-1], trellis[1][1]) + + cell[i - 1][2])
+                cell[i][j] = get_min_num(hamming_distance(input_list[i - 1], trellis[1][0]) + cell[i - 1][0],
+                                         hamming_distance(input_list[i - 1], trellis[1][1]) + + cell[i - 1][2])
             elif j == 2 and i > 1:
-                cell[i][j] = min_num(hamming_distance(input_list[i-1], trellis[2][0]) + cell[i - 1][1],
-                                     hamming_distance(input_list[i-1], trellis[2][1]) + + cell[i - 1][3])
+                cell[i][j] = get_min_num(hamming_distance(input_list[i - 1], trellis[2][0]) + cell[i - 1][1],
+                                         hamming_distance(input_list[i - 1], trellis[2][1]) + + cell[i - 1][3])
             elif j == 3 and i > 1:
-                cell[i][j] = min_num(hamming_distance(input_list[i-1], trellis[3][0]) + cell[i - 1][1],
-                                     hamming_distance(input_list[i-1], trellis[3][1]) + + cell[i - 1][3])
+                cell[i][j] = get_min_num(hamming_distance(input_list[i - 1], trellis[3][0]) + cell[i - 1][1],
+                                         hamming_distance(input_list[i - 1], trellis[3][1]) + + cell[i - 1][3])
+    return cell
+
+
+def decoder(input_str):
+    input_list = input_str.split(" ")
+    trellis = [["00", "11"], ["11", "00"], ["01", "10"], ["10", "01"]]
+    way = [0 for i in range(len(input_list) + 1)]
+    cell = get_cell(input_list, trellis)
     for i in range(len(cell)):
-        way[i] = min_index(cell[i])
-    return way
+        way[i] = get_min_index(cell[i])
+    output_str = ""
+    for i in range(len(way)):
+        if i != 0:
+            if way[i] == 0 or way[i] == 2:
+                output_str = output_str + "0"
+            if way[i] == 1 or way[i] == 3:
+                output_str = output_str + "1"
+    return output_str
 
 
 print(decoder("11 10 01 11 01"))
