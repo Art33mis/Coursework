b6621069bfcbd7b467249a22bca14f6e8d602ac2
Art33mis <artyom12149@gmail.com>
2021-03-17 16:08:16 +0300

    added test

---
+++ viterbi_tests.py
@@ -0,0 +1,17 @@
+import unittest
+import viterbi_dec
+
+
+class MyTestCase(unittest.TestCase):
+    def test_total(self, string="11 01 10 10 10"):
+        test_str = string
+        for char in test_str:
+            if char == '0':
+                char = '1'
+            else:
+                char = "0"
+            self.assertEqual(viterbi_dec.decoder(test_str), "11111")
+
+
+if __name__ == '__main__':
+    unittest.main()
