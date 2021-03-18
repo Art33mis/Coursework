dc69810f4e7893272986cdc28e4a0c33910e9a68
Art33mis <artyom12149@gmail.com>
2021-03-18 20:33:29 +0300

    updated test

--- viterbi_tests.py
+++ viterbi_tests.py
@@ -9,12 +9,13 @@ class MyTestCase(unittest.TestCase):
     def test_total(self, string="11 01 10 10 10"):
         self.assertEqual(viterbi_dec.coder("11111")[:-1], string)
         test_str = string
-        for char in test_str:
-            if char == '0':
-                char = '1'
-            else:
-                char = "0"
+        for i in range(len(test_str)):
+            if test_str[i] == '0':
+                test_str = test_str[:i] + "1" + test_str[i + 1:]
+            if test_str[i] == '1':
+                test_str = test_str[:i] + "0" + test_str[i + 1:]
             self.assertEqual(viterbi_dec.decoder(test_str), "11111")
+            test_str = string
 
 
 if __name__ == '__main__':
