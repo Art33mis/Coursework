46cf1160f4411c4bd0ee13953b46b96add3d2e24
Art33mis <artyom12149@gmail.com>
2021-05-17 22:47:09 +0300

    no strings anymore

--- statistical_dec.py
+++ statistical_dec.py
@@ -5,16 +5,12 @@ import numpy as np
 
 def coder(input_arr):
     """ Basic method which code convolutional code with R = 1/2 """
-    input_str = ''.join(list(map(str, map(chr, np.packbits(input_arr)))))
-    output_str = ""
-    shift_reg = "00"
-    for i in range(len(input_str)):
-        shift_reg = input_str[i] + shift_reg
-        output1 = (int(shift_reg[0]) + int(shift_reg[1]) + int(shift_reg[2])) % 2
-        output2 = (int(shift_reg[0]) + int(shift_reg[2])) % 2
-        output_str = output_str + str(output1) + str(output2) + " "
-    output_list = list(map(str, map(ord, output_str[:-1])))
-    output_arr = np.unpackbits(np.uint8(output_list))
+    shift_reg = np.array([0, 0])
+    output_arr = np.array([], dtype=int)
+    for i in range(len(input_arr)):
+        shift_reg = np.insert(shift_reg, 0, input_arr[i])
+        output_arr = np.append(output_arr, (shift_reg[0] + shift_reg[1] + shift_reg[2]) % 2)
+        output_arr = np.append(output_arr, (shift_reg[0] + shift_reg[2]) % 2)
     return output_arr
 
 
@@ -26,7 +22,7 @@ def get_max_index(cell):
     for i in range(len(cell)):
         if cell[i] > min_num:
             if i != 0:
-                if cell[i] >= cell[i - 1] and cell[i] > max_num:
+                if cell[i] >= cell[i - 1] and cell[i] >= max_num:
                     max_ind = i
                     max_num = cell[i]
                 if cell[i - 1] > cell[i] > max_num:
@@ -37,29 +33,29 @@ def get_max_index(cell):
 
 def get_cell(input_arr, trellis):
     """ This method return trellis diagram """
-    cell = np.array([[0] * 4 for i in range(len(input_arr) + 1)])
+    b = len(input_arr)
+    cell = np.array([[0] * 4 for i in range(int(len(input_arr)/8 + 1))])
     cell[0][0] = 0
     for i in range(len(cell)):
         for j in range(len(cell[i])):
             if j == 0 and i != 0:
-                cell[i][j] = max(mann_whitney(input_arr[i - 1], trellis[0][0]) + cell[i - 1][0],
-                                 mann_whitney(input_arr[i - 1], trellis[0][1]) + cell[i - 1][2])
+                cell[i][j] = max(mann_whitney(input_arr[i - 1:i + 7], trellis[0][0:2]) + cell[i - 1][0],
+                                 mann_whitney(input_arr[i - 1:i + 7], trellis[0][2:]) + cell[i - 1][2])
             elif j == 1 and i != 0:
-                cell[i][j] = max(mann_whitney(input_arr[i - 1], trellis[1][0]) + cell[i - 1][0],
-                                 mann_whitney(input_arr[i - 1], trellis[1][1]) + + cell[i - 1][2])
+                cell[i][j] = max(mann_whitney(input_arr[i - 1:i + 7], trellis[1][0:2]) + cell[i - 1][0],
+                                 mann_whitney(input_arr[i - 1:i + 7], trellis[1][2:]) + + cell[i - 1][2])
             elif j == 2 and i > 1:
-                cell[i][j] = max(mann_whitney(input_arr[i - 1], trellis[2][0]) + cell[i - 1][1],
-                                 mann_whitney(input_arr[i - 1], trellis[2][1]) + + cell[i - 1][3])
+                cell[i][j] = max(mann_whitney(input_arr[i - 1:i + 7], trellis[2][2:]) + cell[i - 1][1],
+                                 mann_whitney(input_arr[i - 1:i + 7], trellis[2][2:]) + + cell[i - 1][3])
             elif j == 3 and i > 1:
-                cell[i][j] = max(mann_whitney(input_arr[i - 1], trellis[3][0]) + cell[i - 1][1],
-                                 mann_whitney(input_arr[i - 1], trellis[3][1]) + + cell[i - 1][3])
+                cell[i][j] = max(mann_whitney(input_arr[i - 1:i + 7], trellis[3][0:2]) + cell[i - 1][1],
+                                 mann_whitney(input_arr[i - 1:i + 7], trellis[3][2:]) + + cell[i - 1][3])
     return cell
 
 
-def st(input_str):
+def st(input_arr):
     """This method creates an input signal for channel"""
-    input_arr = np.array(list(input_str), dtype=int)
-    output_arr = np.zeros(len(input_str)*4)
+    output_arr = np.zeros(len(input_arr)*4)
     for i in range(len(input_arr)):
         output_arr[i*4 + input_arr[i]] = 1
     return output_arr
@@ -67,7 +63,7 @@ def st(input_str):
 
 def rt(input_arr):
     """This method creates an output signal for channel"""
-    output_arr = np.zeros(8)
+    output_arr = np.zeros(len(input_arr))
     for i in range(len(input_arr)):
         r_com1 = np.random.normal(0, 1) + np.random.normal(0, 1)*1j
         r_com2 = np.random.normal(0, 1) + np.random.normal(0, 1)*1j
@@ -79,45 +75,46 @@ def rt(input_arr):
 
 def channel(input_arr):
     """This method imitate channel"""
-    input_str = ''.join(list(map(str, map(chr, np.packbits(input_arr)))))
-    input_arr = np.array(list(input_str.split(" ")))
-    output_arr = np.array([np.zeros(8)]*len(input_arr))
-    for i in range(len(input_arr)):
-        output_arr[i] = rt(st(input_arr[i]))
+    output_arr = np.argsort(np.argsort(rt(st(input_arr))))
     return output_arr
 
 
-def mann_whitney(input_arr, input_str):
+def mann_whitney(input_arr, trellis):
     """Realization of finding Mann-Whitney's criteria"""
-    rank_arr = np.argsort(np.argsort(input_arr))
-    if input_str == "11":
-        crit = rank_arr[1] + rank_arr[5]
-    elif input_str == "00":
-        crit = rank_arr[0] + rank_arr[4]
-    elif input_str == "10":
-        crit = rank_arr[1] + rank_arr[4]
-    elif input_str == "01":
-        crit = rank_arr[0] + rank_arr[5]
+    if trellis[0] == 1:
+        r1 = input_arr[1]
+    else:
+        r1 = input_arr[0]
+    if trellis[1] == 1:
+        r2 = input_arr[5]
+    else:
+        r2 = input_arr[4]
+    crit = r1 + r2
     return crit
 
 
-def decoder(ch_arr):
+def decoder(input_arr):
     """ Basic method which decode convolutional code using Viterbi algorithm """
-    input_arr = np.array(ch_arr)
-    trellis = np.array([["00", "11"], ["11", "00"], ["10", "01"], ["01", "10"]])
-    way = np.array([0 for i in range(len(input_arr) + 1)])
+    trellis = np.array([[0, 0, 1, 1], [1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 0]])
     cell = get_cell(input_arr, trellis)
+    way = np.array([0 for i in range(int(len(input_arr) / 8 + 1))])
     for i in range(len(cell)):
         way[i] = get_max_index(cell[i])
-    output_str = ""
+    output_arr = np.zeros(int(len(input_arr)/8 + 1))
     for i in range(len(way)):
         if i != 0:
             if way[i] == 0 or way[i] == 2:
-                output_str = output_str + "0"
+                output_arr[i] = 0
             if way[i] == 1 or way[i] == 3:
-                output_str = output_str + "1"
-    output_arr = np.unpackbits(np.uint8(list(output_str)))
+                output_arr[i] = 1
+    output_arr = np.delete(output_arr, 0)
     return output_arr
+
+
+#print(coder(np.unpackbits(np.uint8(list(b"11111")))))
+ch = channel(coder(np.array([1, 1, 1, 1, 1])))
+print(f"Результат работы декодирования {decoder(ch)}")
+
 
 
 
